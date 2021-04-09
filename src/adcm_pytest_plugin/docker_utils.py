c71604ee628352166b12f337b0360b48e6bdff60
Dmitry Gusakov <40051639+dgusakov@users.noreply.github.com>
2021-04-09 11:17:06 +0300

    Fix adcm init port when remote docker is involved (#12)
    When we use remote docker it is important to bind containers to the IP of the host with remote docker

--- src/adcm_pytest_plugin/docker_utils.py
+++ src/adcm_pytest_plugin/docker_utils.py
@@ -140,7 +140,17 @@ def get_initialized_adcm_image(
 
 def init_adcm(repo, tag, adcm_repo, adcm_tag, pull, dc=None):
     dw = DockerWrapper(dc=dc)
-    adcm = dw.run_adcm(image=adcm_repo, tag=adcm_tag, remove=False, pull=pull)
+    # Check if we use remote dockerd
+    if dc and "localhost" not in dc.api.base_url:
+        # dc.api.base_url is most likely tcp://{cmd_opts.remote_docker}
+        base_url = dc.api.base_url
+        ip_start = base_url.rfind("/") + 1
+        ip_end = base_url.rfind(":")
+        ip = base_url[ip_start:ip_end]
+    else:
+        # then dc.api.base_url is most likely http+docker://localhost
+        ip = None
+    adcm = dw.run_adcm(image=adcm_repo, tag=adcm_tag, remove=False, pull=pull, ip=ip)
     # Create a snapshot from initialized container
     adcm.container.stop()
     with allure.step(f"Commit initialized ADCM container to image {repo}:{tag}"):
