a1f9adfdde896e2b4f222a78a68f8b907d39d76c
vgorkavenko <gvs@arenadata.io>
2021-05-18 21:39:15 +0400

    Fix '_port_is_free' func
    Due to fact that socket is not closed, this can potentially lead to a false-positive return value

--- src/adcm_pytest_plugin/docker_utils.py
+++ src/adcm_pytest_plugin/docker_utils.py
@@ -46,6 +46,7 @@ class RetryCountExceeded(Exception):
 def _port_is_free(ip, port) -> bool:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     result = sock.connect_ex((ip, port))
+    sock.close()
     if result == 0:
         return False
     return True
