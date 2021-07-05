1400bd9bf37d60de40f302c91b538ae686882a1e
dgusakov <gdv@arenadata.io>
2021-07-05 08:44:31 +0300

    Fix names in dummy bundles

--- src/adcm_pytest_plugin/common.py
+++ src/adcm_pytest_plugin/common.py
@@ -40,20 +40,22 @@ def add_dummy_objects_to_adcm(adcm_client):
     """Add a bunch of dummy objects to ADCM"""
     with allure.step("Create provider"):
         provider_bundle = adcm_client.upload_from_fs(get_data_dir(__file__, "provider"))
-        provider = provider_bundle.provider_prototype().provider_create(name="Some provider")
+        provider = provider_bundle.provider_prototype().provider_create(name="Pre-uploaded provider")
     with allure.step("Create cluster for the further import"):
         cluster_to_import_bundle = adcm_client.upload_from_fs(get_data_dir(__file__, "cluster_to_import"))
-        cluster_to_import = cluster_to_import_bundle.cluster_prototype().cluster_create(name="Dummy cluster to import")
+        cluster_to_import = cluster_to_import_bundle.cluster_prototype().cluster_create(
+            name="Pre-uploaded cluster for the import"
+        )
     with allure.step("Create a cluster with service"):
         cluster_bundle = adcm_client.upload_from_fs(get_data_dir(__file__, "cluster_with_service"))
-        cluster = cluster_bundle.cluster_prototype().cluster_create(name="Cluster with services")
+        cluster = cluster_bundle.cluster_prototype().cluster_create(name="Pre-uploaded cluster with services")
         cluster.bind(cluster_to_import)
     with allure.step("Add services"):
         service_first = cluster.service_add(name="First service")
         service_second = cluster.service_add(name="Second service")
     with allure.step("Add hosts"):
-        host_first = provider.host_create(fqdn="host_in_cluster")
-        host_second = provider.host_create(fqdn="host_in_cluster_second")
+        host_first = provider.host_create(fqdn="pre-uploaded-host-in-cluster")
+        host_second = provider.host_create(fqdn="pre-uploaded-host-in-cluster-second")
         cluster.host_add(host_first)
         cluster.host_add(host_second)
     with allure.step("Set components"):
