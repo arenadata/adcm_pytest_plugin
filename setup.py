282875ebda42aac0570523ec1cd8b6ac7588c75b
skhomuti <skhomuti@gmail.com>
2021-10-25 15:30:10 +0500

    ADCM-2123 fix container config

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
-    version="4.10.2",
+    version="4.10.3",
     # the following makes a plugin available to pytest
     entry_points={"pytest11": ["adcm_pytest_plugin = adcm_pytest_plugin.plugin"]},
     # custom PyPI classifier for pytest plugins
