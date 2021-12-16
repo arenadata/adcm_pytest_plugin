5f011649c25aa67e3ec75e81ec30dff4de3b93d8
Sealwing <egaraslanoff@yandex.ru>
2021-12-16 16:24:33 +0500

    ADCM-2372 Second provider created for dummy data (API tests new requirement for complex foreign keys

--- src/adcm_pytest_plugin/common.py
+++ src/adcm_pytest_plugin/common.py
@@ -43,6 +43,11 @@ def add_dummy_objects_to_adcm(adcm_client):
     with allure.step("Create provider"):
         provider_bundle = adcm_client.upload_from_fs(get_data_dir(__file__, "provider"))
         provider = provider_bundle.provider_prototype().provider_create(name="Pre-uploaded provider")
+    with allure.step("Create second provider and some hosts"):
+        second_provider_bundle = adcm_client.upload_from_fs(get_data_dir(__file__, "second_provider"))
+        second_provider = second_provider_bundle.provider_create(name="Second provider")
+        for i in range(6):
+            second_provider.host_create(fqdn=f"second-provider-host-{i}")
     with allure.step("Create cluster for the further import and add hosts to it"):
         cluster_to_import_bundle = adcm_client.upload_from_fs(get_data_dir(__file__, "cluster_to_import"))
         cluster_to_import = cluster_to_import_bundle.cluster_prototype().cluster_create(
