18ff83ced547768fb01692f9525d43c43914116c
Araslanov Egor <egaraslanoff@yandex.ru>
2022-06-09 09:09:07 +0500

    ADCM-2863 Function to call "logrotate" ADCM command added (#94)
    * ADCM-2863 Function to call "logrotate" ADCM command added
    
    * ADCM-2863 Options fixed

--- src/adcm_pytest_plugin/steps/commands.py
+++ src/adcm_pytest_plugin/steps/commands.py
@@ -14,11 +14,15 @@
 import os.path
 import subprocess
 from contextlib import contextmanager
-from typing import List, Generator, Tuple
+from typing import List, Generator, Tuple, Optional, Literal
 
 import allure
 
 from adcm_pytest_plugin.docker_utils import ADCM, is_file_presented_in_directory
+
+
+_ACTIVATE_DEFAULT_VENV = "source /adcm/venv/default/bin/activate"
+_RUN_MANAGE_PY = "python3 /adcm/python/manage.py"
 
 
 @allure.step('Run ADCM command "dumpcluster" on cluster {cluster_id} to file {file_path}')
@@ -27,13 +31,13 @@ def dump_cluster(adcm: ADCM, cluster_id: int, file_path: str, password: str) -> 
     Dump cluster with "dumpcluster" command.
     """
     command = "dumpcluster"
-    arguments = _prepare_cmd_arguments(adcm, f"python3 /adcm/python/manage.py {command} -c {cluster_id} -o {file_path}")
+    arguments = _prepare_cmd_arguments(adcm, f"{_RUN_MANAGE_PY} {command} -c {cluster_id} -o {file_path}")
     dump_dir, dump_file = os.path.dirname(file_path), os.path.basename(file_path)
     with _run_in_subprocess(arguments) as process:
         stdout, stderr = _type_password(process, password)
     if file_path in stdout and is_file_presented_in_directory(adcm.container, dump_file, dump_dir):
         return
-    _command_failed(command, stdout, stderr)
+    _interactive_command_failed(command, stdout, stderr)
 
 
 @allure.step('Run ADCM command "loadcluster" from file {file_path}')
@@ -42,12 +46,49 @@ def load_cluster(adcm: ADCM, file_path: str, password: str) -> None:
     Load cluster with "loadcluster" command.
     """
     command = "loadcluster"
-    arguments = _prepare_cmd_arguments(adcm, f"python3 /adcm/python/manage.py {command} {file_path}")
+    arguments = _prepare_cmd_arguments(adcm, f"{_RUN_MANAGE_PY} {command} {file_path}")
     with _run_in_subprocess(arguments) as process:
         stdout, stderr = _type_password(process, password)
     if "Load successfully ended" in stdout:
         return
-    _command_failed(command, stdout, stderr)
+    _interactive_command_failed(command, stdout, stderr)
+
+
+def logrotate(
+    adcm: ADCM, target: Optional[Literal["all", "job", "config", "nginx"]] = None, disable_logs: bool = False
+):
+    """
+    Run rotation of logs (job/config/nginx) with "logrotate" command.
+    """
+    step_message = 'Run ADCM command "logrotate"'
+    options = []
+    if target is not None:
+        options.append(f"--target {target}")
+    if disable_logs:
+        options.append("--disable-logs")
+    if options:
+        step_message += f"with options: {' '.join(options)}"
+
+    with allure.step(step_message):
+        command = "logrotate"
+        exit_code, output = _run_with_docker_exec(
+            adcm,
+            [
+                "sh",
+                "-c",
+                f"{_ACTIVATE_DEFAULT_VENV} && {_RUN_MANAGE_PY} {command} {' '.join(options) if options else ''}",
+            ],
+        )
+        if exit_code == 0:
+            return
+        _docker_exec_command_failed(command, exit_code, output)
+
+
+def _run_with_docker_exec(adcm: ADCM, args: List[str]) -> Tuple[int, bytes]:
+    """
+    Execute command given in arguments with `exec_run`
+    """
+    return adcm.container.exec_run(args)
 
 
 def _prepare_cmd_arguments(adcm: ADCM, command: str) -> List[str]:
@@ -61,7 +102,7 @@ def _prepare_cmd_arguments(adcm: ADCM, command: str) -> List[str]:
         adcm.container.name,
         "sh",
         "-c",
-        f"source /adcm/venv/default/bin/activate && {command}",
+        f"{_ACTIVATE_DEFAULT_VENV} && {command}",
     ]
 
 
@@ -90,10 +131,22 @@ def _type_password(process: subprocess.Popen, password: str) -> Tuple[str, str]:
     return stdout.decode("utf-8"), stderr.decode("utf-8")
 
 
-def _command_failed(command: str, stdout_log: str, stderr_log: str):
+def _interactive_command_failed(command: str, stdout_log: str, stderr_log: str):
     """
     Attach logs of command and raise AssertionError
+    when interactive command execution (ran with Popen) failed
     """
     allure.attach(stdout_log, name="stdout", attachment_type=allure.attachment_type.TEXT)
     allure.attach(stderr_log, name="stderr", attachment_type=allure.attachment_type.TEXT)
     raise AssertionError(f'Command "{command}" failed.\nCheck attachments for more details')
+
+
+def _docker_exec_command_failed(command: str, exit_code: int, output: bytes):
+    """
+    Attach logs of command and raise AssertionError
+    when command execution (ran with `docker_exec`) failed
+    """
+    allure.attach(
+        output.decode("utf-8"), name="command output (from `docker_exec`)", attachment_type=allure.attachment_type.TEXT
+    )
+    raise AssertionError(f'Command "{command}" failed with {exit_code} exit code.\nCheck attachments for more details.')
